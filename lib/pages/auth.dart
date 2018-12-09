2e4993887268b3a35d8cf2f2886f4e45747f974d
Zach Wolpe <zachcolinwolpe@gmail.com>
2018-12-09 15:42:39 +0200

    update

---
+++ lib/pages/auth.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/ui_elements/app_bar.dart';
+
+class AuthPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: MyAppBar(),
+
+    );
+  }
+}
---
+++ lib/widgets/ui_elements/app_bar.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class MyAppBar extends AppBar {
+  
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text('Health cial', style: TextStyle(
+        fontFamily: 'Comfortaa'
+      ),),
+    );
+  }
+}
