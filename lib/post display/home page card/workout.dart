55b59b49cd5b3701213213f5ccb7b40123d6a5bc
Zach Wolpe <zachcolinwolpe@gmail.com>
2018-12-22 14:09:35 +0200

    git

--- lib/post display/home page card/workout.dart
+++ lib/post display/home page card/workout.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating/flutter_rating.dart';
 
 // Import Post Types
 import '../../post types/workout_post.dart';
@@ -24,18 +25,35 @@ class WorkoutPostHomePage extends StatefulWidget {
 }
 
 class _WorkoutProstHomePageState extends State<WorkoutPostHomePage> {
-  Widget _starRating() {
-    return GestureDetector(
-      // add gesture detector
-      child:  Row(
-        children: <Widget>[
-          Icon(Icons.star, size: 50),
-          Icon(Icons.star, size: 50),
-          Icon(Icons.star, size: 50),
-          Icon(Icons.star_half, size: 50),
-          Icon(Icons.star_border, size: 50),
-        ],
-      ),
+  double _rating = 3.5;
+
+  Widget _userRating() {
+    return StarRating(
+      starCount: 5,
+      size: 40,
+      rating: _rating,
+      borderColor: Colors.grey[900],
+      color: Colors.grey[900],
+      onRatingChanged: (rating) {
+        setState(() {
+          this._rating = rating;
+        });
+      },
+    );
+  }
+
+  Widget _averageRating() {
+    return StarRating(
+      starCount: 5,
+      size: 40,
+      rating: 4,
+      borderColor: Colors.transparent,
+      color: Colors.cyan[200],
+      onRatingChanged: (rating) {
+        // setState(() {
+        //   this._rating = rating;
+        // });
+      },
     );
   }
 
@@ -62,16 +80,22 @@ class _WorkoutProstHomePageState extends State<WorkoutPostHomePage> {
             Positioned(
                 child: ProfilePicCircle(
                     widget._post.userDetails.userProfilePicture)),
-            Positioned(
-              bottom: 5,
-              right: 5,
-              child: _starRating(),
-            )
           ],
         ),
-        SizedBox(height: 5),
-        KeyMetricsBar(widget._post),
-        SizedBox(height: 5),
+        Stack(
+          children: <Widget>[
+            _averageRating(),
+            _userRating(),
+            Positioned(
+              bottom: 3,
+              right: 12,
+              child: Text(
+                '4754 votes',
+                style: TextStyle(fontSize: 10, fontWeight: FontWeight.w700),
+              ),
+            )
+          ],
+        )
       ],
     );
   }
